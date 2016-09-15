8733ec2d4235e616bec0035b7e58f0e93790ddbd
App Academy Student <student@appacademy.io>
2016-09-15 14:11:39 -0700

    Populated tile.rb with #initialize, #flip!, #mark!, and #to_s

--- board.rb
+++ board.rb
@@ -1,6 +1,33 @@
 class Board
 
-  def initialize
+  def initialize(grid_size = 9)
+    @grid = Array.new(grid_size) {Array.new(grid_size)}
+    populate_grid
+  end
+
+  def [](pos)
+    @grid[pos[0]][pos[1]]
+  end
+
+  def []=(pos,val)
+    @grid[pos[0]][pos[1]] = val
+  end
+
+  def populate_grid
+    seed_bombs
+    populate_values
+  end
+
+  def seed_bombs
+    num_bomb = grid_size+1
+    until num_bomb == 0
+      x = rand(@grid.length)
+      y = rand(@grid.length)
+      if @grid[]
+    end
+  end
+
+  def populate_values
   end
 
 end
--- tile.rb
+++ tile.rb
@@ -1,6 +1,30 @@
 class Tile
 
-  def initialize
+  def initialize(value)
+    @value = value
+    @flipped = false
+    @marked = false
+    if value == :b
+      @bomb_flag = true
+    else
+      @bomb_flag = false
+    end
+  end
+
+  def flip!
+    if @flipped == false
+      @fliped = true
+    else
+      raise 'Cannot flip a previously flipped tile!'
+    end
+  end
+
+  def mark!
+    @marked = !@marked
+  end
+
+  def to_s
+    @value.to_s
   end
 
 end
