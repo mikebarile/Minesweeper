8511315f565a010fb9e4511985d69b93f2008681
App Academy Student <student@appacademy.io>
2016-09-15 13:40:23 -0700

    Initial commit

---
+++ minesweeper.rb
@@ -0,0 +1,24 @@
+class Minesweeper
+  attr_accessor :board
+  def initialize(board = Array.new(9){Array.new(9)})
+    @board = board
+    populate_bombs(@board)
+  end
+
+  def flag_bomb()
+
+  end
+
+  def populate_bombs(board)
+
+  end
+
+  def reveal_square()
+
+  end
+
+  def take_turn()
+
+  end
+
+end
